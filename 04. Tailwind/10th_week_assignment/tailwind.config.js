751f8db0d98041aee1e7f436d9cc1621230b088d
Ashuwani kumar <ashukaushik1007@gmail.com>
2023-07-20 16:37:00 +0530

    Upload 11th week assignement

---
+++ 04. Tailwind/10th_week_assignment/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./10th_week_assignment/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- 04. Tailwind/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./10th_week_assignment/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
