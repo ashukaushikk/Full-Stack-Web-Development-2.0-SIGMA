ccf6d3de281567843fcd21a8d4a6dfe9bfad7524
ashu kaushik <ashukaushik1007@gmail.com>
2023-09-28 15:03:17 +0530

    Updated

--- 05. JavaScript/15th_week_assignment/JS_DOM/Question-1/script.js
+++ 05. JavaScript/15th_week_assignment/JS_DOM/Question-1/script.js
@@ -1,10 +1,10 @@
 // Fetching Elements : =>>>
-let imageInput = document.querySelector('input');
-let displayerBox = document.querySelector('.displayer');
-let image = document.querySelector('img');
+let imageInput = document.querySelector("input");
+let displayerBox = document.querySelector(".displayer");
+let image = document.querySelector("img");
 
 // Logic : =>>>
-imageInput.addEventListener('change', (event) => {
+imageInput.addEventListener("change", (event) => {
   let file = event.target.files[0];
 
   if (file) {
@@ -17,3 +17,22 @@ imageInput.addEventListener('change', (event) => {
     reader.readAsDataURL(file);
   }
 });
+
+// // 2nd Way:
+
+// // Fetching Elements : =>>>
+// const imageInput = document.querySelector(`#image`);
+// const previewImage = document.querySelector(`#preview-image`);
+
+// // Logic : =>>>
+// inputImage.addEventListener("change", (event) => {
+//   if (event.target.files.length == 0) {
+//     return;
+//   }
+//   const file = event.target.files[0];
+//   const reader = new FileReader();
+//   reader.onload = () => {
+//     previewImage.src = reader.result;
+//   };
+//   reader.readAsDataURL(file);
+// });
--- 05. JavaScript/15th_week_assignment/JS_DOM/Question-2/script.js
+++ 05. JavaScript/15th_week_assignment/JS_DOM/Question-2/script.js
@@ -1,14 +1,15 @@
 // Fetching Elements : =>>>
-let body = document.querySelector('body');
-let heading = document.querySelector('h1');
-let toggleBtn = document.querySelector('button');
+const body = document.querySelector(`body`);
+const heading = document.querySelector(`h1`);
+const btn = document.querySelector(`#toggle`);
 
 // Adding Event Listner : =>>>
-toggleBtn.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  if (body.getAttribute('class') === 'dark-mode') {
-    heading.textContent = 'This is Dark mode';
+btn.addEventListener("click", (event) => {
+  btn.classList.toggle(`active`);
+  body.classList.toggle(`dark-mode`);
+  if (body.getAttribute("class") === `dark-mode`) {
+    heading.textContent = "This is Dark mode";
   } else {
-    heading.textContent = 'This is Light mode';
+    heading.textContent = "This is Light mode";
   }
 });
